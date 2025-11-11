dd7645ec67257cf83d22c4e80d9ec7ec538e248f
maheenrasool <123101752+maheenrasool@users.noreply.github.com>
2025-11-11 22:55:56 +0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
-from app.app import hello
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # noqa: E402
+
+from app import hello
 
 
 def test_hello():
