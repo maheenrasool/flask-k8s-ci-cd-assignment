cbeff0a76241cdeb36f644af126a1362b6efe10b
sofiaRaheel <151sofiaraheel@gmail.com>
2025-11-12 20:27:58 +0500

    test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,9 @@
-# tests/test_app.py
+# tests/test_app.py.
 
 from app import hello
-from app.app import app
 
 
-def test_hello_function(capsys):
+def test_hello(capsys):
     hello()
     captured = capsys.readouterr()
     assert captured.out == "Hi\n"
-
-
-def test_flask_homepage():
-    client = app.test_client()
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b"Hello, World from Flask CI/CD!" in response.data
